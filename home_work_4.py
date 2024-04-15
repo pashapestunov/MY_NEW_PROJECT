1d18b96de4c249debf9f466105925fe54f4d47fc
pashapestunov <pestunov.pasha9999@gmail.com>
2024-04-15 22:50:00 +0300

    LA-DD54 i have four new files

---
+++ home_work_4.py
@@ -0,0 +1,4 @@
+chars = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print("Сумма списка chars =", sum(chars))
+print("Максимальное число в списке chars =", max(chars))
+print("Минимально число в списке chars =", min(chars))
