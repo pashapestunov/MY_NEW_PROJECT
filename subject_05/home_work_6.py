00b417d80898244d9fe7f6aa2eda54803b4735ac
pashapestunov <pestunov.pasha9999@gmail.com>
2024-05-16 18:51:58 +0300

    LA-DD54 there are new files

--- subject_05/home_work_6.py
+++ subject_05/home_work_6.py
@@ -8,34 +8,12 @@
 
 numbers = [2, 15, 3, 5, 9, 1, 7, 12]
 numbers.sort()
-print(numbers)
-
-index_1 = numbers.index(2)
-index_2 = numbers.index(15)
-index_3 = numbers.index(3)
-index_4 = numbers.index(5)
-index_5 = numbers.index(9)
-index_6 = numbers.index(1)
-index_7 = numbers.index(7)
-index_8 = numbers.index(12)
+print(f'Дан список: {numbers}')
 
 number = int(input("Введите число из списка: "))
 
-if number == 2:
-    print("Число", number, "есть в списке под индексом", index_1)
-elif number == 15:
-    print("Число", number, "есть в списке под индексом", index_2)
-elif number == 3:
-    print("Число", number, "есть в списке под индексом", index_3)
-elif number == 5:
-    print("Число", number, "есть в списке под индексом", index_4)
-elif number == 9:
-    print("Число", number, "есть в списке под индексом", index_5)
-elif number == 1:
-    print("Число", number, "есть в списке под индексом", index_6)
-elif number == 7:
-    print("Число", number, "есть в списке под индексом", index_7)
-elif number == 12:
-    print("Число", number, "есть в списке под индексом", index_8)
+if number in numbers:
+    index = numbers.index(number)
+    print(f'Число {number} есть в списке под индексом, {index}')
 else:
-    print("Такого числа нет")
+    print("Такого числа нет в списке")
--- subject_05/home_work_7.py
+++ subject_05/home_work_7.py
@@ -4,24 +4,25 @@
 список [5, 6, 7, 1, 2, 3, 4])
 """
 
+
 def find_num(lst, search_num):
     low = 0
     high = len(lst) - 1
-    search_res = False
 
-    while low <= high and not search_res:
+    while low <= high:
         middle = (low + high) // 2
         guess = lst[middle]
         if guess == search_num:
-            result_srh = True
-            return result_srh
-        if guess > search_num:
+            return True
+        elif guess > search_num:
             high = middle - 1
         else:
             low = middle + 1
-    return search_res
+    return False
 
-lst = [2, 13, 15, 24, 66, 76, 88, 99]
+
+lst = [2, 99, 15, 45, 65, 66, 88, 13]
+lst.sort()
 value = int(input("Введите число для поиска: "))
 
 result = find_num(lst, value)
