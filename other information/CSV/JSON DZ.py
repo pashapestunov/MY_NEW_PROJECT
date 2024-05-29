a46e291c5d5d2eacfcc020cc3fc4cba8fc41876e
pashapestunov <pestunov.pasha9999@gmail.com>
2024-05-29 22:01:13 +0300

    Merge remote-tracking sqlite3

--- other information/CSV/JSON DZ.py
+++ other information/CSV/JSON DZ.py
@@ -1,8 +1,17 @@
 import json
-import jpype
 
 
-with open('employees.json', 'r') as json_file:
-    data = json.load(json_file)
+try:
+    with open('employees.json', 'r') as json_file:
+        data = json.loads(json_file.read())
+    output = ','.join([*data[0]])
+    print(output)
+    for obj in data:
+        output += f'\n{obj["name"]},{obj["birthday"]},{obj["height"]}, {obj["weight"]}, {obj["car"]}, {obj["languages"]}'
+    print(output)
+    with open('employees.csv', 'w') as csv_file:
+        csv_file.write(output)
 
-    print(type(data), data)
+except Exception as ex:
+    print(f"Error: {str(ex)}")
+
