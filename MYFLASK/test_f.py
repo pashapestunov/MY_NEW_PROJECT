da11ed9a995cea605427cacd3258e66715ed6626
pashapestunov <pestunov.pasha9999@gmail.com>
2024-06-05 23:24:32 +0300

    Merge remote-tracking sqlite3

--- MYFLASK/test_f.py
+++ MYFLASK/test_f.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from flask import request
 
 app = Flask(__name__)
@@ -15,9 +15,25 @@ app = Flask(__name__)
 #     return (render_template("test_f1.html", name=name))
 #
 
+
 @app.route('/')  # отслеживаем главную страницу
 def index():
     return render_template("test_f1.html")
+
+
+@app.route('/test_f1_enter.html')
+def test_f1_enter():
+    return render_template("test_f1_enter.html")
+
+
+@app.route('/test_f1.html')
+def back_from_enter():
+    return render_template("test_f1.html")
+
+
+@app.route('/test_f1_registration.html')
+def test_f1_registration():
+    return render_template("test_f1_registration.html")
 
 
 if __name__ == "__main__":  # мы указываем, что этот файл основной
