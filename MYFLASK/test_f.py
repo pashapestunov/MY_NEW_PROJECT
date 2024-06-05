de106f0a7d20560990c5d58633d261ca985b2a41
pashapestunov <pestunov.pasha9999@gmail.com>
2024-06-05 14:04:57 +0300

    Merge remote-tracking sqlite3

--- MYFLASK/test_f.py
+++ MYFLASK/test_f.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template, url_for
+from flask import request
 
 app = Flask(__name__)
 
+# @app.route('/submit', methods=['POST'])
+# def submit():
+#   name = request.form['name']
+#   sex = request.form['sex']
+#   return f'Hello, {name}, {sex}'
+#
+# @app.route('/hello/')
+# @app.route('/hello/<name>')  # отслеживаем главную страницу
+# def hello(name=None):
+#     return (render_template("test_f1.html", name=name))
+#
 
 @app.route('/')  # отслеживаем главную страницу
 def index():
